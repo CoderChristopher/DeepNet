3898d0dd3b241568a9b2f873c505fd8775d898d4
Christopher Copeland <cmc232@zips.uakron.edu>
2021-02-14 15:18:37 -0500

    Simplified RNG seed variable, Cleaned out some old signal catching code.

--- src/main.c
+++ src/main.c
@@ -34,46 +34,31 @@ void CalculateScore(struct actor* player){
 	ResetEntryPoint();
 	return;
 }
-
-//This is not POSIX COMPLIANT... >:D
-//But at the same time... If I am catching a segmentation fault and the program is going
-//to exit anyway because a seg fault is non recoverable why does it matter if I modify
-//the global state of the program? Like at this point things have already gone to hell
-//in handbasket, I am just trying to get some useful information and stop ncurses so
-//that it isn't a complete shit show
 void SegmentationFaultCatch(int signo){
 	endwin();
 	printf("There was segmentation fault in the program at %i,%i.\n",entrypoint,preventrypoint);
 	printf("Please email this above error to copelandwebdesign@gmail.com .\n",entrypoint,preventrypoint);
 	exit(1);
 }
-//Ok, I have no excuse for this one...Maybe I will rewrite the function to be posix
-//compliant
-void IntCatch(int signo){
-	//TODO Write some code that interprets these errors... eh I am too lazy to do it
-	//now and there is more interesting code to write...
-	return;
-}
 void CloseCatch(int signo){
 	endwin();
 	CalculateScore(player);
 	exit(1);
 }
-//End of depravity
 
 int main(int argc,void* argv[]){
 	BOOLEAN seedset=FALSE;
-	int seedto=-1;
-	if(argc>1)
-	if(strstr(argv[1],"-s")){
-		char* val=strtok("-s",argv[1]);
-		if(val){
-			seedto=atoi(val);
-			seedset=TRUE;
-		}else{
-			if(argc>=3){
-				seedto=atoi(argv[2]);
-				seedset=TRUE;
+	time_t seed=-1;
+	if(argc>1){
+		if(strstr(argv[1],"-s")){
+			char* val=strtok("-s",argv[1]);
+			if(val){
+				seed=atoi(val);
+			}else{
+				if(argc>=3){
+					seed=atoi(argv[2]);
+					seedset=TRUE;
+				}
 			}
 		}
 	}
@@ -83,11 +68,11 @@ int main(int argc,void* argv[]){
 	signal(SIGSEGV,SegmentationFaultCatch);
 	signal(SIGINT ,CloseCatch);
 	signal(SIGTERM,CloseCatch);
-	signal(SIGKILL,CloseCatch);
 	signal(SIGQUIT,CloseCatch);
 
-	//Setup Ncurses
 	SetEntryPoint(EP_MAIN);
+
+	//Setup Ncurses
 	NCursesInit();
 	NColorPairs();
 
@@ -97,14 +82,13 @@ int main(int argc,void* argv[]){
 	Update=&MENU_Update;
 
 	//RNG Init
-	time_t seed=time(NULL);
-	if(seedset)
-		seed=seedto;
+	if(!seedset)
+		seed=time(NULL);
 	srand(seed);
 
 	//Setup looks
 	NewLook('@',COL_GR,"Entity Locator","Indicates the location of outside entity.","");
-	NewLook('!',COL_GR,"Data Fragment","This is a collectable data fragment.","Usually these a word some Crypto Cred");
+	NewLook('!',COL_GR,"Data Fragment","This is a collectable data fragment.","Usually these award some Crypto Cred");
 	NewLook('$',COL_GR,"Crypto Cred","Crypto creds is money that can be used to buy things.","");
 	NewLook('%',COL_RD,"ECM Software","This is entity counter measure software that may be equipped for attack or defense.","");
 	NewLook('+',COL_RD,"Access Gate","Access gates monitor for unathorized user access and will trace any suspicious activity.","");
@@ -126,7 +110,7 @@ int main(int argc,void* argv[]){
 	//Main Game Loop
 	Render();
 	do{
-		ProcessInput();//Process Input
+		ProcessInput();
 		Update();
 		Render();//Render the world
 	}while(!done);//Check to see if quit condition is called, if not continue on
@@ -149,4 +133,3 @@ int main(int argc,void* argv[]){
 
 	return 0;
 }
-
